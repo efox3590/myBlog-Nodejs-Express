0723764d59ab3fb5a42adad46e374ae6cd9a56c0
Emily Fox <emily.r.fox@hotmail.com>
2017-03-19 15:27:31 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
+const http = require('http');
+const express = require('express');
+const routes = require('./routes');
+const api = require('./routes/api');
+const path = require('path');
 
-/**
- * Module dependencies.
- */
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const errorHandler = require('errorhandler');
 
-const express = require('express'),
-  routes = require('./routes'),
-  api = require('./routes/api');
+const app = express();
 
-const app = module.exports = express.createServer();
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'uwotm8' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {
-    layout: false
-  });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
-
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+// error handling middleware should be loaded after the loading the routes
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
 
 // Routes
 
@@ -40,7 +42,6 @@ app.get('/partials/:name', routes.partials);
 // JSON API
 
 app.get('/api/posts', api.posts);
-
 app.get('/api/post/:id', api.post);
 app.post('/api/post', api.addPost);
 app.put('/api/post/:id', api.editPost);
@@ -49,8 +50,7 @@ app.delete('/api/post/:id', api.deletePost);
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
-// Start server
-
-app.listen(3000, function(){
-  console.log("Server listening on 3000");
+const server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
